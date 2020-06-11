9194a6e3127fb45a3b99561061e3cafe31c34714
Florry <viktor.soderstrom@live.se>
2020-06-11 14:59:42 +0200

    adds some descriptions

--- schemas/DecodeTokenRequest.js
+++ schemas/DecodeTokenRequest.js
@@ -1,5 +1,5 @@
 module.exports = {
 	id: "DecodeTokenRequest",
-	description: "The token to decode.",
+	description: "The token to decode. To work optimally headers from the initial request should be sent in the headers of this request. This to be able to save things as `user-agent` & `version` in the session of the current user.",
 	type: "string"
 };
